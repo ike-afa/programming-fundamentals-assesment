b6c756f972bf8b9e0719490dd81c7468a817b591
Ike Afanasiev <ike@tosh.me>
2025-05-16 11:54:51 +1000

    added gameplay loop

--- hangman.py
+++ hangman.py
@@ -7,23 +7,31 @@ class Game:
         file.close()
         index = random.randint(0, len(words) - 1)
         self.word = words[index]
+        self.bad_guesses = []
+        self.attempt_letters = ['_' for c in self.word]
+    
+    def play(self):
+        guesses=[]
+        while not self.game_over():
+            guess = ""
+            while len(guess)!=1 or guess<'a' or guess>'z':
+                guess = input("whats your guess? ")
+            guesses.append(guess)
+            print(guesses)
+            
+    
+    def attempt(self):
+        return "".join(self.attempt_letters)
+    
+    def game_over(self):
+        return len(self.bad_guesses) >= 8 or self.attempt() == self.word
 
 def main():
     
     game = Game('names.txt')
     
-
-    print(game.word)
-    return
-        
-
-    guesses=[]
-    guess = input("whats your guess? ")
-    while len(guess)!=1 or guess<'a' or guess>'z':
-        guess = input("whats your guess? ")
-    guesses.append(guess)
-    print(guesses)
-
+    game.play()
+    
 if __name__ == '__main__':
     main()
 
