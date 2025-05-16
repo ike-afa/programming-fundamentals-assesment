ee36153c85980273ff4c17f7f472df37462baa0f
Ike Afanasiev <ike@tosh.me>
2025-05-16 13:08:52 +1000

    made working hangman

--- hangman.py
+++ hangman.py
@@ -7,17 +7,34 @@ class Game:
         file.close()
         index = random.randint(0, len(words) - 1)
         self.word = words[index]
-        self.bad_guesses = []
+        self.bad_guesses = set()
         self.attempt_letters = ['_' for c in self.word]
+        # temporary debugging
+        print(f"the word is {self.word}")
     
     def play(self):
-        guesses=[]
         while not self.game_over():
             guess = ""
             while len(guess)!=1 or guess<'a' or guess>'z':
                 guess = input("whats your guess? ")
-            guesses.append(guess)
-            print(guesses)
+            
+            # was this a good guess?
+            if guess in self.word:
+                for i in range(0, len(self.word)):
+                    if guess == self.word[i]:
+                        self.attempt_letters[i] = guess
+            else:
+                self.bad_guesses.add(guess)
+                
+           
+            print(self.attempt())
+            print(f"bad guesses: {self.bad_guesses}")
+        
+        if self.attempt() == self.word:
+            print(f"you won")
+        else:
+            print(f"too many bad guesses")
+    
             
     
     def attempt(self):
