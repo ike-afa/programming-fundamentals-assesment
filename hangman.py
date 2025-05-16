0f0db54a73869d4d830c3d530de84990b6374c6f
Ike Afanasiev <ike@tosh.me>
2025-05-16 11:19:10 +1000

    working on hangman

--- hangman.py
+++ hangman.py
@@ -1,3 +1,22 @@
-file=open('file.txt','r')
-words=file.read().splitlines()
+import random
+
+file = open('file.txt','r')
+words = file.read().splitlines()
 file.close()
+
+index = random.randint(0, len(words) - 1)
+word = words[index]
+
+print(word)
+
+guesses=[]
+guess = input("whats your guess? ")
+while len(guess)!=1 or guess<'a' or guess>'z':
+    guess = input("whats your guess? ")
+guesses.append(guess)
+print(guesses)
+
+    
+
+
+
