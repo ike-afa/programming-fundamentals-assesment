a38a70292bf7e02f07b3c54bf22ffe58ddb11786
Ike Afanasiev <ike@tosh.me>
2025-05-17 11:26:59 +1000

    end of step 2 in maze

--- maze.py
+++ maze.py
@@ -8,6 +8,17 @@ class cell:
     topConnected=False
     bottomConnected=False
 
+def newMaze(numRows, numCols):
+    maze = []
+    for row in range(0, numRows):
+        rowArray = []
+        for col in range(0, numCols):
+            c = cell()
+            c.row = row
+            c.col = col
+            rowArray.append(c)
+        maze.append(rowArray)
+    return maze
 
 
 if __name__ == "__main__":
@@ -15,3 +26,6 @@ if __name__ == "__main__":
     print(c.visited)
     c.visited = True
     print(c.visited)
+
+    maze = newMaze(20, 20)
+    
