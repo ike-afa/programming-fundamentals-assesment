f6e1564a6850a2844b587fe18e026952909433c6
Ike Afanasiev <ike@tosh.me>
2025-05-20 19:27:39 +1000

    finding unvisited neighbour

--- maze.py
+++ maze.py
@@ -1,3 +1,5 @@
+import random
+
 class cell:
 
     row=None
@@ -38,3 +40,43 @@ def showMaze(numRows, numCols, maze):
         print(topLine + "+")
         print(cellLine + "|")
     print("+-"*(numCols)+"+")
+        
+def findNeighbours(row, col, rows, cols, maze):
+    '''finds all neighbours of a cell'''
+    neighbours = []
+    for i in range(-1, 2):
+       # add i to row
+       if row + i < 0 or row + i >= rows:
+           continue
+       
+       for j in range(-1, 2):
+           # add j to col
+           if col + j < 0 or col + j >= cols:
+               continue
+           
+           if i == 0 and j == 0:
+               # skip target cell
+               continue
+
+           neighbour = maze[row+i][col+j]
+           neighbours.append(neighbour)
+
+    return neighbours
+
+def unvisitedNeighbour(row, col, rows, cols, maze):
+    '''return a random unvisited neighbour (or None)'''
+    neighbours = findNeighbours(row, col, rows, cols, maze)
+    unvisited = []
+
+    for neighbour in neighbours:
+        if not neighbour.visited:
+            unvisited.append(neighbour)
+
+    if len(unvisited) == 0:
+        return None
+    else:
+        randomIndex = random.randint(0, len(unvisited)-1)
+        return unvisited[randomIndex]      
+           
+
+    
